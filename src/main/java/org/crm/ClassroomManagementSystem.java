96291c006a35ec9a513fc0dff1ad0aff98b0dc93
zelalem-fisseha <zelalem.fisseha@bitscollege.edu.et>
2024-12-12 23:05:29 +0300

    this is are the library that we need and also i have made the connection with the database

--- src/main/java/org/crm/ClassroomManagementSystem.java
+++ src/main/java/org/crm/ClassroomManagementSystem.java
@@ -1,4 +1,14 @@
 package org.crm;
+import org.crm.model.Datasource;
+
+import java.sql.Connection;
 
 public class ClassroomManagementSystem {
+    public static void main(String[] args) {
+        Datasource datasource = new Datasource();
+        Connection connection = datasource.connect();
+        if (connection != null) {
+            System.out.println("Connection established");
+        }
+    }
 }
--- src/main/java/org/crm/model/Datasource.java
+++ src/main/java/org/crm/model/Datasource.java
@@ -1,4 +1,38 @@
 package org.crm.model;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 public class Datasource {
+    String URL="jdbc:postgresql://localhost:5432/cclassmanagementtt";
+    String USER="zolafss";
+    String PASSWORD="kill123qwe";
+
+    Connection con = null;
+    public Connection connect() {
+
+        try{
+            con= DriverManager.getConnection(URL,USER,PASSWORD);
+            System.out.println("Connection established with database");
+            return con;
+        }catch ( SQLException e) {
+            System.out.println("Failed to connect to database" + e.getMessage());
+            return null;
+        }
+    }
+    public void disconnect() {
+        try {
+            if(con!=null){
+                con.close();
+                System.out.println("Connection closed");
+            }
+        }catch(SQLException e){
+            System.out.println("Failed to close connection");
+        }
+
+    }
+
+
 }
